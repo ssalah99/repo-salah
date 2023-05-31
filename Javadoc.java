11446f911a580237b3e913fab84768ab3d377d41
ssalah99 <135035942+ssalah99@users.noreply.github.com>
2023-05-31 23:40:34 +0200

    Add files via upload

---
+++ Javadoc.java
@@ -0,0 +1,16 @@
+package entornos;
+
+public class Javadoc {
+	//Salah El Mokadem
+
+	/**
+	 * Este método realiza una tarea específica.
+	 * @param parametro1 Descripción del primer parámetro.
+	 * @param parametro2 Descripción del segundo parámetro.
+	 * @return Descripción del valor devuelto.
+	 */
+	public int miMetodo(int parametro1, String parametro2) {
+	    // Código del método aquí
+	}
+
+}
